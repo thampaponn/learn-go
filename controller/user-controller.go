21ddd316015f38e4faae71dd79fb590edf89c336
Tun <thampapon.tn@gmail.com>
2025-07-13 00:21:53 +0700

    (fix): update user service

--- controller/user-controller.go
+++ controller/user-controller.go
@@ -31,6 +31,12 @@ func (c controller) Save(ctx *gin.Context) entity.User {
 	var user entity.User
 	if err := ctx.ShouldBindJSON(&user); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return entity.User{}
 	}
-	return c.service.Save(user)
+	user, err := c.service.Save(user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return entity.User{}
+	}
+	return user
 }
--- service/user-service.go
+++ service/user-service.go
@@ -1,13 +1,15 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/thampaponn/learn-go/database"
 	"github.com/thampaponn/learn-go/entity"
 )
 
 type UserService interface {
 	FindAll() []entity.User
-	Save(entity.User) entity.User
+	Save(entity.User) (entity.User, error)
 }
 
 type userService struct {
@@ -23,7 +25,21 @@ func (service *userService) FindAll() []entity.User {
 	return users
 }
 
-func (service *userService) Save(user entity.User) entity.User {
-	database.DB.Create(&user)
-	return user
+func (service *userService) Save(user entity.User) (entity.User, error) {
+	// Check required fields
+	if user.FirstName == "" || user.LastName == "" || user.Username == "" || user.Password == "" {
+		return entity.User{}, errors.New("missing required field(s)")
+	}
+
+	// Check for duplicate username
+	var existing entity.User
+	if err := database.DB.Where("username = ?", user.Username).First(&existing).Error; err == nil {
+		return entity.User{}, errors.New("username already exists")
+	}
+
+	// Save user
+	if err := database.DB.Create(&user).Error; err != nil {
+		return entity.User{}, err
+	}
+	return user, nil
 }
